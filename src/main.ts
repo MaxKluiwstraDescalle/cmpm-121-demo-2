40d2d29c0108cd5ba5da6f5790fd1083569696fc
Max-Emilien Kluiwstra-Descalle <mkluiwst@ucsc.edu>
2024-10-17 11:07:43 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import "./style.css";
 
-const APP_NAME = "Sketchpad Assignement";
+//const APP_NAME = "Sketchpad Assignement";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//document.title = APP_NAME;
+//app.innerHTML = APfP_NAME;
+
+
+const gameName = "Sketchpad";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.id = 'drawingCanvas';
+
+document.body.appendChild(canvas);
